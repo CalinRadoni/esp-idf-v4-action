5362b567c0945761d55e1c3c5e72e8bec81fff99
Calin Radoni <CalinRadoni@users.noreply.github.com>
2020-10-12 23:01:32 +0300

    Tests build.sh paths

--- build.sh
+++ build.sh
@@ -5,7 +5,14 @@ source ~/esp/esp-idf/export.sh
 src_directory=${INPUT_SRC_DIRECTORY:-.}
 idf_reconfigure=${INPUT_IDF_RECONFIGURE:-true}
 
+echo $INPUT_SRC_DIRECTORY
+echo $src_directory
+pwd
+ls -al
+
 cd "$src_directory"
+pwd
+ls -al
 
 if [[ "$idf_reconfigure" == "true" ]]; then
   echo -e "\n\n## Reconfigure the project"
